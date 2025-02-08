85243569639b3c2db274f096fec4a3c4cb68d9f5
AhmedDSalim <155827291+AHMEDDSALIM@users.noreply.github.com>
2025-02-08 23:14:04 +0300

    fix location marker

--- src/components/GeoMap.jsx
+++ src/components/GeoMap.jsx
@@ -1,7 +1,16 @@
 import React, { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
+import iconlocation from "../assets/icon-location.svg";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-
+const customIcon = L.icon({
+  iconUrl: iconlocation, // Replace with the path to your custom icon
+  iconSize: [25, 41], // Size of the icon
+  iconAnchor: [12, 41], // Point of the icon which will correspond to marker's location
+  popupAnchor: [1, -34], // Point from which the popup should open relative to the iconAnchor
+  shadowSize: [41, 41], // Optional: size of the shadow
+  shadowAnchor: [12, 41], // Optional: point of the shadow which will correspond to marker's location
+});
 function GeoMap(props) {
   const loc = props.info.loc.split(",").map(Number);
 
@@ -20,7 +29,7 @@ function GeoMap(props) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={loc}></Marker>
+      <Marker position={loc} icon={customIcon}></Marker>
     </MapContainer>
   );
 }
